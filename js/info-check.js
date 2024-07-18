8c686f0a7843322f91ea0c806134a36d8540786f
SeungAh-Yoo99 <ysa8497@gmail.com>
2024-07-18 11:49:20 +0900

    ADD: 인증 요청 정보 확인 팝오버 show 구현

--- js/info-check.js
+++ js/info-check.js
@@ -1,4 +1,13 @@
+// 인증 요청 버튼
+const authenticationRequestButton = document.getElementById('authenticationRequestButton');
 // 정보 입력 안내 팝오버
 const infoWarningPopover = document.getElementById('infoWarningPopover');
 
-infoWarningPopover.showPopover();
+
+// 인증 요청 버튼 클릭 시 정보 유효성 검사
+authenticationRequestButton.addEventListener('click', (event) => {
+
+  // if(true) {
+  //   infoWarningPopover.classList.add('show');
+  // }
+})
