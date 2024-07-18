b71c05d686eb19d168c33bab08ac9cb4a10fe5ae
seonmin5 <seonmin5555@gmail.com>
2024-07-18 10:46:19 +0900

    Update icon click -> name change

--- js/iconclick.js
+++ js/iconclick.js
@@ -4,9 +4,6 @@
 const appList = document.getElementsByClassName("app-icon");
 const appListArray = Array.from(appList);
 
-// // appIcon 클래스를 가진 요소 선택
-// const appIconElement = document.querySelector(".app-icon");
-
 // 각 app-icon 요소에 클릭 이벤트 리스너 추가
 appListArray.forEach((iconElement) => {
   iconElement.addEventListener("click", () => {
@@ -18,6 +15,10 @@ appListArray.forEach((iconElement) => {
     const imgSrc = imgElement.src;
     const imgAlt = imgElement.alt;
 
+    // 클릭된 아이콘의 이름 가져오기
+    const iconNameElement = iconElement.querySelector("a");
+    const iconName = iconNameElement ? iconNameElement.textContent : "";
+
     // 이미지 생성
     let iconImage = document.createElement("img");
     iconImage.src = imgSrc;
@@ -25,9 +26,19 @@ appListArray.forEach((iconElement) => {
     iconImage.style.width = "30px";
     iconImage.style.height = "30px";
     iconImage.style.borderRadius = "10px";
+    iconImage.style.marginLeft = "5px";
 
-    // right-main header 이미지 추가
-    rightMainHeader.innerHTML = "";
-    rightMainHeader.appendChild(iconImage);
+    // 이름 생성
+    let iconNameSpan = document.createElement("span");
+    iconNameSpan.textContent = iconName;
+    iconNameSpan.style.fontFamily = "wooriB";
+    iconNameSpan.style.fontSize = "17px";
+    iconNameSpan.style.marginLeft = "10px";
+    iconNameSpan.style.color = "#FFFFFF";
+
+    // right-main header 이미지 및 이름 추가
+    rightMainHeader.innerHTML = ""; // 기존 내용 삭제
+    rightMainHeader.appendChild(iconImage); // 새로운 이미지 추가
+    rightMainHeader.appendChild(iconNameSpan); // 이름 추가
   });
 });
