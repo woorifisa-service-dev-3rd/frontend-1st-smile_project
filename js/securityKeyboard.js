a7b7c528e62439abe3d81a99e46eb9ce836c1b42
seonmin5 <seonmin5555@gmail.com>
2024-07-18 13:42:51 +0900

    Update security keyboard modal close button img

--- js/securityKeyboard.js
+++ js/securityKeyboard.js
@@ -1,1 +1,18 @@
 // input.birth click -> security keyboard
+
+// input.birth click ->  security keyboard 모달창 띄우기
+const inputBirthElement = document.querySelector("#birth");
+const modal = document.querySelector("#modal");
+const closeButton = document.querySelector(".button-close");
+const screen = document.querySelector(".screen");
+
+inputBirthElement.addEventListener("click", showModal);
+closeButton.addEventListener("click", closeModal);
+
+function showModal(params) {
+  modal.style.display = "block";
+}
+
+function closeModal(params) {
+  modal.style.display = "none";
+}
